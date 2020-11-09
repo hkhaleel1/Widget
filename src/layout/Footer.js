29a7ca41dc8df8463dcd96500e1801dd689da563
hamza-civica <hamza.khaleel@civica.co.uk>
2020-11-09 09:36:28 +0000

    add html loader

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {ReactComponent as RightArrow} from '../images/further-reading-arrow--grey.svg'
+import RightArrow from '../images/further-reading-arrow--grey.svg'
 
 export default function Footer({ text }) {
     return (
         <div className="widgetFooter" >
             <span className="footerText">{text}</span>
             <span className="arrowContainer" >
-                <RightArrow className="arrow"/>
+                <img src={RightArrow} alt="rightArrow" className="arrow"/>
             </span>
         </div>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
-const webpack = require('webpack');
-module.exports = {
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = { 
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.svg$/,
-          use: [
+        rules: [
             {
-              loader: 'svg-url-loader',
-              options: {
-                limit: 10000,
-              },
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
-          ],
-        },
-        {
-            test: /\.css$/i,
-            use: ['css-loader'],
-        },
-      ]
-    }
-  };
+            {
+              test: /\.html$/,
+              use: [
+                {
+                  loader: "html-loader"
+                }
+              ]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
