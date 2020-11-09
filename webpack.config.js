fd065b55239a79ddf14aebdd6a932dc8ec26d960
hamza-civica <hamza.khaleel@civica.co.uk>
2020-11-09 08:55:22 +0000

    add bable and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+      ]
+    }
+  };
